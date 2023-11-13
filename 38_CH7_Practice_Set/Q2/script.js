2ba9b99613994cac5ed0410dbb69e64c37aa2ff9
MD MUSTAQIM ALAM <134624079+Mustaqim-Alam@users.noreply.github.com>
2023-11-13 11:18:27 +0530

    Delete 38_CH7_Practice_Set directory

--- 38_CH7_Practice_Set/Q2/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName("div")[0].firstElementChild.style.color = "green"
-document.getElementsByTagName("div")[0].lastElementChild.style.color = "green"
-
-
-// Q4
-Array.from(document.getElementsByTagName("li")).forEach((element) => {
-    element.style.background = "cyan"
-});
