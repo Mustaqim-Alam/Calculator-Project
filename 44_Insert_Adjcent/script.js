1c64f20b2aba85b664578c75443b8add94c8c7a2
MD MUSTAQIM ALAM <134624079+Mustaqim-Alam@users.noreply.github.com>
2023-11-13 11:19:09 +0530

    Delete 44_Insert_Adjcent directory

--- 44_Insert_Adjcent/script.js
+++
@@ -1,4 +0,0 @@
-firstid.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
-firstid.insertAdjacentHTML('beforebegin', '<div class="test">beforebegain</div>');
-firstid.insertAdjacentHTML('afterbegin','<div class"test">afterbegin</div>')
-firstid.insertAdjacentHTML('afterend','<div class"test">afterend</div>')
